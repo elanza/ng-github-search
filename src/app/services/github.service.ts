69ab51921cba7dd9f8007e3fc035124654b42d8b
lanzer <lanzer@lanzers-MacBook-Pro.local>
2016-10-28 12:08:42 -0400

    https test

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -17,11 +17,11 @@ export class GithubService {
   // Getting info from Github API.
 
   getUser(){
-    return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret).map(res => res.json());
+    return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret).map(res => res.json());
   }
 
   getRepos(){
-    return this._http.get('http://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
+    return this._http.get('https://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
       .map(res => res.json());
   }
 
